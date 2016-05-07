6088f9f7b8c7fd6af292b4bbce1c99f926409519
Maria Athena Beatrice Engesaeth <maria.enges@gmail.com>
2016-05-07 12:50:09 +0100

    complete analysis

--- healthcare_analytics/Analysis/stat_analysis_healthcare.R
+++
@@ -1,9 +0,0 @@
-# Read from .csv
-setwd("/Users/mariaathena/Dropbox (Personal)/00 Imperial College/1602 Healthcare & Medical Analytics/Assignment (individual)/Data (csv)")
-data.health <- read.csv("parsed_healthcare_data.csv")
-head(data.health)
-
-######## ######## ######## ######## ######## ######## ######## ######## ######## 
-# PREDICTIVE MODEL FOR READMISSION
-
-model
